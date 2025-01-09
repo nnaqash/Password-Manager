b6bd27819747758e55146369e355c9f9333a44d9
nnaqash <kashisohtra@gmail.com>
2025-01-09 19:33:41 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+     <Manager/>
+    </>
+  )
+}
+
+export default App
+/* 13:55 */
---
+++ src/components/Manager.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Manager = () => {
+  return (
+    <>
+    <div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    </div>
+
+    <div className="container mx-auto bg-slate-700">
+        <div className="text-white flex flex-col p-4">
+            <input type="text" name="" id="" />
+            <div className="flex">
+                <input type="text"  />
+                <input type="text"  />
+            </div>
+        </div>
+    </div>
+    
+    </>
+    
+  )
+}
+
+export default Manager
